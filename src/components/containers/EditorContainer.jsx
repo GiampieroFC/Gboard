01a3d905d87a9642acd9adfc48b4f1e172726348
GiampieroFC <giampieroferminicastillo@gmail.com>
2023-02-16 11:43:19 +0300

    added scroll to change size and button to clean text

--- src/components/containers/EditorContainer.jsx
+++ src/components/containers/EditorContainer.jsx
@@ -8,7 +8,7 @@ import { useEffect } from "react"
 
 function EditorContainer() {
 
-    const [color, setColor] = useState("#ffffff")
+    const [color, setColor] = useState("#00ff00")
     const [size, setSize] = useState("18")
 
     function handlerColor(color) {
@@ -17,8 +17,10 @@ function EditorContainer() {
         document.getElementsByTagName('textarea')[0].style.color = color
     }
     function handlerSize(size) {
+
         setSize(size)
         document.getElementsByTagName('textarea')[0].style.fontSize = `${size}px`
+
     }
 
     useEffect(() => {
@@ -28,7 +30,9 @@ function EditorContainer() {
 
     function copy() {
         navigator.clipboard.writeText(document.querySelector('textarea').value)
+    }
 
+    function download(params) {
         const textToWrite = document.querySelector('textarea').value;
         const textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
         const fileNameToSaveAs = "apuntes-clase-espanol.txt";
@@ -44,13 +48,39 @@ function EditorContainer() {
         downloadLink.click();
     }
 
+    function clean() {
+        document.querySelector('textarea').value = ''
+    }
+
+    function handlerWheel(e) {
+
+        if (e.deltaY < 0) {
+            let size = parseInt(e.target.value) + 4
+            console.log(size)
+            handlerSize(size)
+        }
+        if (e.deltaY > 0) {
+            let size = parseInt(e.target.value) - 4
+            console.log(size)
+            handlerSize(size)
+        }
+    }
+
 
     return (
         <>
             <div className="controls">
-                <input type="range" min="8" max="200" step="1" value={size} onChange={e => handlerSize(e.target.value)} />
+                <input type="range"
+                    min="8" max="200"
+                    step="1"
+                    onWheel={handlerWheel}
+                    value={size}
+                    onChange={e => handlerSize(e.target.value)}
+                />
                 <input type="color" value={color} onChange={e => handlerColor(e.target.value)} />
-                <button onClickCapture={copy}>Copiar</button>
+                <button onClickCapture={copy}>Copy</button>
+                <button onClickCapture={download}>Download</button>
+                <button onClickCapture={clean}>Clean</button>
             </div>
             <TextBox dfv={"\n"} />
         </>
