15d01969e1c5486bc38c68933b31fa912506e915
GiampieroFC <giampieroferminicastillo@gmail.com>
2023-03-03 23:59:14 +0300

    implementing pwa

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import App from './App'
 import './App.css'
 import store from "./app/store.js";
 import { Provider } from "react-redux";
+import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
+
+    <Provider store={store}>
+        <App />
+    </Provider>,
 )
+
+const updateSW = registerSW({
+    onNeedRefresh() { },
+    onOfflineReady() { },
+})
